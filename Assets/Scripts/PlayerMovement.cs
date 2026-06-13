6f4c48189e729b2dab10c789c5ba0dc6acc30424
lengochung1111 <265447928+lengochung1111@users.noreply.github.com>
2026-06-13 19:32:49 +0700

    Initial check-in

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,53 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+    public float gridSize = 1f;
+    private bool isMoving;
+    private Vector2 input;
+    [SerializeField] private InputActionReference moveAction;
+
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.action.Disable();
+    }
+
+    void Update()
+    {
+        if (!isMoving)
+            HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        input = moveAction.action.ReadValue<Vector2>();
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            input = new Vector2(Mathf.Sign(input.x), 0);
+            if (input == Vector2.zero) return;
+
+        Vector3 targetPos = transform.position + (Vector3)input * gridSize;
+        StartCoroutine(Move(targetPos));
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        
+        transform.position = targetPos;
+        isMoving = false;
+
+    }
 }
